ce115854b5e1c140e54ce7a338ac50bb3821621b
Tedy Saputra <99706457+teddysaputra610@users.noreply.github.com>
2022-04-09 09:50:17 +0700

    latihan

--- latihan/bin/section_16/lib/main.dart
+++ latihan/bin/section_16/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,79 +6,127 @@ void main() {
   runApp(const MyApp());
 }
 
+const String kontak = "/kontak";
+const String kontakBaru = "/kontakBaru";
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Live Sesion 16',
+      title: 'Section 16',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Kontak(),
       routes: {
-       kontak: (_) => const Kontak(),
-       KontakBaru:(_) => const KontakBaru()
+        kontak: (_) => const Kontak(),
+        kontakBaru: (_) => KontakBaru(),
       },
-      initialRoute: Kontak,
+      initialRoute: kontak,
     );
   }
 }
+
 class Kontak extends StatelessWidget {
-  const Kontak({ Key? key }) : super(key: key);
+  const Kontak({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Faker faker = Faker();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Contac'),
+        title: const Text("Contacts"),
         centerTitle: true,
-      ),
-      body: ListView.separated(itemBuilder: ((context, index) => ListTile(
-        leading: CircleAvatar(
-          radius: 30,
-          backgroundImage: NetworkImage(
-            faker.image.image(),
-          ),
-        ),
-        title: Text(
-          faker.person.name(),
-        ),
-        subtitle: ,
-      )), separatorBuilder: (separatorBuilder), itemCount: itemCount)
-    );
-  }
-}
-class KontakBaru extends StatelessWidget {
-  const KontakBaru({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('New Contac'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pushNamed(context, 'kontakBaru')
-        } ),
-      body: Form(
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                label:  Text('Nomor HP'),
-                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15)))
+          Navigator.pushNamed(context, kontakBaru);
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.separated(
+          itemBuilder: (ctx, index) => ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(
+                    faker.image.image(),
+                  ),
+                ),
+                title: Text(
+                  faker.person.name(),
+                ),
+                subtitle: Text(faker.phoneNumber.us()),
               ),
-              keyboardType: TextInputType.phone,
-              inputFormatters: <TextInputFormatter> [
-                FilteringTextInputFormatter.digitsOnly
-              ],
-            )
-          ],
-        ) ),
+          separatorBuilder: (ctx, index) {
+            return const Divider();
+          },
+          itemCount: 25),
+    );
+  }
+}
+
+class KontakBaru extends StatelessWidget {
+  KontakBaru({Key? key}) : super(key: key);
+
+  TextEditingController inputNama = TextEditingController();
+  TextEditingController inputHp = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create New Contact"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          TextFormField(
+            controller: inputNama,
+            decoration: const InputDecoration(
+              label: Text("Name"),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+            ),
+          ),
+          TextFormField(
+            controller: inputHp,
+            decoration: const InputDecoration(
+              label: Text("Number Phone"),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+            ),
+            keyboardType: TextInputType.phone,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (samting) {
+                    return AlertDialog(
+                      content: Column(
+                        children: [
+                          Text("Nama     : ${inputNama.text}"),
+                          Text("Nomor Hp : ${inputHp.text}"),
+                        ],
+                      ),
+                    );
+                  });
+            },
+            child: const Text("Submit"),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ latihan/bin/section_19/android/app/src/main/kotlin/com/example/section_19/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.section_19
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
